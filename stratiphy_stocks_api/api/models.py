5f21612064f6ef8b26dfca1b17d2ae20567248d9
Reuben Hughes <rh15406@bristol.ac.uk>
2023-05-06 17:54:56 +0100

    Create the stock model and load some initial data into the table

--- stratiphy_stocks_api/api/models.py
+++ stratiphy_stocks_api/api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Stock(models.Model):
+    stock_id = models.AutoField(primary_key=True)
+    stock_name = models.CharField(max_length=100)
+    short_code = models.CharField(max_length=10)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.stock_name
