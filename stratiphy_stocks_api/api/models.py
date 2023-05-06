4d389a73008a3578b9d526fc77c03a955fea12cf
Reuben Hughes <rh15406@bristol.ac.uk>
2023-05-06 19:46:48 +0100

    Add quantity to stock table, introduce initial models for UserProfile (admin/investor) and UserStock table to track stocks for each user

--- stratiphy_stocks_api/api/models.py
+++ stratiphy_stocks_api/api/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Stock(models.Model):
@@ -6,6 +7,19 @@ class Stock(models.Model):
     stock_name = models.CharField(max_length=100)
     short_code = models.CharField(max_length=10)
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.IntegerField()
 
     def __str__(self):
         return self.stock_name
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user_type = models.CharField(max_length=8)
+
+
+class UserStock(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    stock = models.ForeignKey(Stock, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+
--- stratiphy_stocks_api/api/serializers.py
+++ stratiphy_stocks_api/api/serializers.py
@@ -5,7 +5,7 @@ from .models import Stock
 class StockSerializer(serializers.ModelSerializer):
     class Meta:
         model = Stock
-        fields = ['stock_id', 'stock_name', 'short_code', 'price']
+        fields = ['stock_id', 'stock_name', 'short_code', 'price', 'quantity']
 
     def to_representation(self, instance):
         representation = super().to_representation(instance)
