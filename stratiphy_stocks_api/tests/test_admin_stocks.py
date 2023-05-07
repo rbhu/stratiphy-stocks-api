cbc9af0b353b37b4fd7e19518b8685695f3ef9b3
Reuben Hughes <rh15406@bristol.ac.uk>
2023-05-07 14:10:32 +0100

    Add tests for all different admin functionalities

--- stratiphy_stocks_api/tests/test_admin_stocks.py
+++ stratiphy_stocks_api/tests/test_admin_stocks.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.test import TestCase
+from rest_framework import status
 from rest_framework.test import APIClient
 
 from api.models import Stock, UserProfile
@@ -11,17 +12,14 @@ class AdminStockViewSetTestCase(TestCase):
     def setUp(self):
         self.permission = IsInvestor()
         self.client = APIClient()
-        self.stock1 = Stock.objects.create(stock_id=1, stock_name='Stock 1', price=10, short_code='STK1', quantity=10)
-        self.stock2 = Stock.objects.create(stock_id=2, stock_name='Stock 2', price=20, short_code='STK2', quantity=10)
 
-        # Create a user with 'admin' user type
         self.admin_user = User.objects.create_user(username='admin', password='password')
         self.admin_profile = UserProfile.objects.create(user=self.admin_user, user_type='admin')
-
-        # Authenticate the client with the admin user
         self.client.force_authenticate(user=self.admin_user)
 
     def test_list_stocks(self):
+        Stock.objects.create(stock_id=1, stock_name='Stock 1', price=10, short_code='STK1', quantity=10)
+        Stock.objects.create(stock_id=2, stock_name='Stock 2', price=20, short_code='STK2', quantity=10)
         response = self.client.get('/stock-api/admin/stocks/')
         stocks = Stock.objects.all()
         serializer = StockSerializer(stocks, many=True)
@@ -29,8 +27,64 @@ class AdminStockViewSetTestCase(TestCase):
         self.assertEqual(response.data, serializer.data)
 
     def test_retrieve_stock_by_id(self):
+        Stock.objects.create(stock_id=1, stock_name='Stock 1', price=10, short_code='STK1', quantity=10)
         response = self.client.get('/stock-api/admin/stocks/1', {}, True)
         stock = Stock.objects.get(stock_id=1)
         serializer = StockSerializer(stock)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data, serializer.data)
+
+    def test_create_stock(self):
+        data = {
+            'stock_name': 'Stock',
+            'short_code': 'STK',
+            'price': 10.0,
+            'quantity': 100
+        }
+        response = self.client.post('/stock-api/admin/stocks/', data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Stock.objects.count(), 1)
+        stock = Stock.objects.get(short_code='STK')
+        self.assertEqual(stock.stock_name, data['stock_name'])
+        self.assertEqual(stock.short_code, data['short_code'])
+        self.assertEqual(stock.price, data['price'])
+        self.assertEqual(stock.quantity, data['quantity'])
+
+    def test_update_stock(self):
+        stock = Stock.objects.create(stock_name='Stock', short_code='STK', price=10.0, quantity=100)
+
+        data = {
+            'stock_name': 'New Stock Name',
+            'short_code': 'NSTK',
+            'price': 15.0,
+            'quantity': 200
+        }
+
+        response = self.client.put(f'/stock-api/admin/stocks/{stock.stock_id}/', data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        stock.refresh_from_db()
+        self.assertEqual(stock.stock_name, data['stock_name'])
+        self.assertEqual(stock.price, data['price'])
+        self.assertEqual(stock.quantity, data['quantity'])
+
+    def test_partial_update_stock(self):
+        stock = Stock.objects.create(stock_name='Stock', short_code='STK', price=10.0, quantity=100)
+
+        data = {
+            'price': 15.0,
+            'quantity': 200
+        }
+
+        response = self.client.patch(f'/stock-api/admin/stocks/{stock.stock_id}/', data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        stock.refresh_from_db()
+        self.assertEqual(stock.price, data['price'])
+        self.assertEqual(stock.quantity, data['quantity'])
+
+    def test_delete_stock(self):
+        stock = Stock.objects.create(stock_name='Stock', short_code='STK', price=10.0, quantity=100)
+
+        response = self.client.delete(f'/stock-api/admin/stocks/{stock.stock_id}/')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Stock.objects.count(), 0)
+
